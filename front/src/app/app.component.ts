eab8eab549a2b84354cf8cd2c26aa4bf9e14e4a5
Ivan <ivanetflix977@gmail.com>
2024-03-24 13:32:42 +0100

    Estilos globales de tailwind

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'front';
